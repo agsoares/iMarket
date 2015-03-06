e432794d6612fb6f508e6e8ddf4676bfecbf9e64
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2015-03-06 16:50:04 -0300

    New List View inserted

---
+++ iMarket/iMkt_NewListViewController.h
@@ -0,0 +1,15 @@
+//
+//  iMkt_NewListViewController.h
+//  iMarket
+//
+//  Created by Jonathan Andrade on 06/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iMkt_NewListViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *txtListName;
+
+@end
---
+++ iMarket/iMkt_NewListViewController.m
@@ -0,0 +1,16 @@
+//
+//  iMkt_NewListViewController.m
+//  iMarket
+//
+//  Created by Jonathan Andrade on 06/03/15.
+//  Copyright (c) 2015 Bepid. All rights reserved.
+//
+
+#import "iMkt_NewListViewController.h"
+
+@implementation iMkt_NewListViewController
+
+- (IBAction)createNewList:(id)sender {
+    
+}
+@end
