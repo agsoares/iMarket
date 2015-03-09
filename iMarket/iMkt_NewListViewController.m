5e421b4ca5d54fd0d9508dc8c3c90dd76de69625
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2015-03-09 16:12:08 -0300

    New list view: keyboard return function and keyboard disappear

--- iMarket/iMkt_NewListViewController.m
+++ iMarket/iMkt_NewListViewController.m
@@ -11,7 +11,29 @@
 @implementation iMkt_NewListViewController
 
 - (IBAction)createNewList:(id)sender {
-  _nameOfList = _txtListName.text;
-  [self performSegueWithIdentifier:@"NewListUnwindSegue" sender:sender];
+    if ([_txtListName.text length] != 0)
+    {
+      _nameOfList = _txtListName.text;
+      [self performSegueWithIdentifier:@"NewListUnwindSegue" sender:sender];
+    }
+    else{
+        UIAlertView *alertEmptyListNameField = [[UIAlertView alloc] initWithTitle:@"Empty list name" message:@"Please, insert a name for your list." delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles: nil];
+        [alertEmptyListNameField show];
+    }
 }
+
+- (IBAction)hideKeyboard:(id)sender {
+    [self.view endEditing:YES];
+}
+
+-(BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+    if ([textField isEqual: _txtListName])
+    {
+        [self createNewList:nil];
+    }
+    
+    return YES;
+}
+
 @end
